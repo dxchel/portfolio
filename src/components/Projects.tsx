5a5ff5a567fca4a01393e9cc6263377ff9ae6058
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-12-16 23:45:29 -0600

    Placeholder on Project section as it needs more work

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,8 @@
 import { Container, Nav, Tab, Row, Col } from "react-bootstrap";
 import logo from "../assets/img/logo.svg";
 import { ProjectCard } from './ProjectCard';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
 
 export const Projects = () => {
     const projects: { title: string, description: string, imgUrl: string }[] = [
@@ -33,42 +35,51 @@ export const Projects = () => {
 
     return(
         <section className="project" id="projects">
-            <Container>
+            <h1>PROJECTS WIP</h1>
+            <h3>Please reach out using the three social network buttons in the top right.</h3>
+            {/*<Container>
                 <Row>
-                    <Col>
-                        <h2>Projects</h2>
-                        <p>I've worked on several areas of engineering, ranging from software, electrical and even mechanical engineering. Here are some of the most important.</p>
-                        <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                            <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first">Work</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second">College</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="third">Personal</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
-                            <Tab.Pane eventKey="first">
-                                <Row>
-                                    {
-                                        projects.sort(() => Math.random() - 0.5).map((project, index) => {
-                                            return (<ProjectCard key={index} {...project} />)
-                                        })
-                                    }
-                                </Row>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="second">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                            </Tab.Pane>
-                            <Tab.Pane eventKey="third">
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                            </Tab.Pane>
-                        </Tab.Container>
+                    <Col size={12}>
+                        <TrackVisibility>
+                            {({ isVisible }) =>
+                            <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                                <h2>Projects</h2>
+                                <p>I've worked on several areas of engineering, ranging from software, electrical and even mechanical engineering. Here are some of the most important.</p>
+                                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="first">Work</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="second">College</Nav.Link>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <Nav.Link eventKey="third">Personal</Nav.Link>
+                                        </Nav.Item>
+                                    </Nav>
+                                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                                        <Tab.Pane eventKey="first">
+                                            <Row>
+                                                {
+                                                    projects.sort(() => Math.random() - 0.5).map((project, index) => {
+                                                        return (<ProjectCard key={index} {...project} />)
+                                                    })
+                                                }
+                                            </Row>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="second">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                        </Tab.Pane>
+                                        <Tab.Pane eventKey="third">
+                                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                                        </Tab.Pane>
+                                    </Tab.Content>
+                                </Tab.Container>
+                            </div>}
+                        </TrackVisibility>
                     </Col>
                 </Row>
-            </Container>
+            </Container>*/}
         </section>
     )
 }
