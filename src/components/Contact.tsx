4462b970f97545e0f7fa19a49de182c7b0d15b77
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-12-16 23:45:01 -0600

    Fixed contact section for static page usage

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import contactImg from "../assets/img/logo.svg";
+import connectImg from "../assets/img/connect.png";
 
 export const Contact = () => {
     const formInitialDetails = {
@@ -11,60 +11,44 @@ export const Contact = () => {
         message: ''
     }
     const [formDetails, setFormDetails] = useState(formInitialDetails);
-    const [buttonText, setButtonText] = useState('Send');
-    const [status, setStatus] = useState({success: false, message: ""});
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        let response = await fetch("http://localhost:5000/contact", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json;charset=utf-8",
-            },
-            body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = await response.json();
-        setFormDetails(formInitialDetails);
-        if (result.code === 200) {
-            setStatus({ success: true, message: 'Message sent successfully'});
-        } else {
-            setStatus({ success: false, message: 'Something went wrong, please try again later.'});
-        }
+        const name: string = formDetails.firstName + " " + formDetails.lastName;
+        const email: string = formDetails.email;
+        const phone: string = formDetails.phone;
+        let message: string = formDetails.message;
+        if(message === '') message = `Hi David Xchel!\n\nI saw your amazingly super duper portfolio and wanted to reach out! please contact me, my mail is ${email}${phone !== '' ? " and my phone is " + phone : ''}.\n\nThanks!\n${name}`;
+        window.location.href = `mailto:davidxchelmh@gmail.com?subject=${name} reaching from your personal portfolio&body=${message}`;
     };
+
     return (
         <section className="contact" id="connect">
             <Container>
                 <Row className="align-items-center">
                     <Col md={6}>
-                    <img src={contactImg} alt="Contact me"/>
+                    <img className="connect-img" src={connectImg} alt="Contact me"/>
                     </Col>
                     <Col>
                         <h2>Get in touch</h2>
                         <form onSubmit={handleSubmit}>
                             <Row>
                                 <Col sm={6} className="px-1">
-                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => setFormDetails({ ...formDetails, firstName: e.target.value })}/>
+                                <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => setFormDetails({ ...formDetails, firstName: e.target.value })} required/>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => setFormDetails({ ...formDetails, lastName: e.target.value })}/>
+                                <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => setFormDetails({ ...formDetails, lastName: e.target.value })} required/>
                                 </Col>
                                 <Col sm={6} className="px-1">
-                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => setFormDetails({ ...formDetails, email: e.target.value })}/>
+                                <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => setFormDetails({ ...formDetails, email: e.target.value })} required/>
                                 </Col>
                                 <Col sm={6} className="px-1">
                                 <input type="tel" value={formDetails.phone} placeholder="Phone" onChange={(e) => setFormDetails({ ...formDetails, phone: e.target.value })}/>
                                 </Col>
                                 <Col>
                                 <textarea rows={6} value={formDetails.message} placeholder="Message" onChange={(e) => setFormDetails({ ...formDetails, message: e.target.value })}/>
-                                    <button type="submit"><span>{buttonText}</span></button>
+                                    <button type="submit"><span>Mail</span></button>
                                 </Col>
-                                {
-                                    status.message &&
-                                    <Col>
-                                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                                    </Col>
-                                }
                             </Row>
                         </form>
                     </Col>
