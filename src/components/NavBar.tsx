a0be1bef622c25756873b19efaaa1928b45a299c
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-12-16 20:38:26 -0600

    Added NavBar routing fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import logo from '../assets/img/logo.svg';
 import socialLI from '../assets/img/socialLI.svg';
 import socialGH from '../assets/img/socialGH.svg';
 import socialGM from '../assets/img/socialGM.svg';
@@ -24,9 +23,9 @@ export const NavBar = () => {
     return (
         <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
             <Container>
-                <Navbar.Brand href="#home">
+                {/*<Navbar.Brand href="#home">
                     <img src={logo} alt="logo" />
-                </Navbar.Brand>
+                </Navbar.Brand>*/}
                 <Navbar.Toggle aria-controls="responsive-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
@@ -36,15 +35,16 @@ export const NavBar = () => {
                         <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => setActiveLink('skills')}>Skills</Nav.Link>
                         <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => setActiveLink('projects')}>Projects</Nav.Link>
                     </Nav>
-                    <span className="navbar-text">
+                    <span className={"navbar-text"}>
                         <div className="social-icon">
                             <a href="https://www.linkedin.com/in/david-xchel-morales-hurtado"><img src={socialLI} alt="LinkedIn"/></a>
                             <a href="https://www.github.com/dxchel"><img src={socialGH} alt="GitHub"/></a>
                             <a href="mailto:davidxchelmh@gmail.com"><img src={socialGM} alt="Mail"/></a>
                         </div>
                         <Nav.Link href="#connect">
-                            <button className="vvd"><span>Let's connect!</span></button>
+                            <button className="nav-connect"><span>Let's connect!</span></button>
                         </Nav.Link>
+                        <a href='../assets/David_Xchel_Morales_Hurtado-CV.pdf' download><button className="nav-resume"><span>Resume</span></button></a>
                     </span>
                 </Navbar.Collapse>
             </Container>
