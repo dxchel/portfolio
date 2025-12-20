cb46b7ab7e4249a01ef6fb5dc4d770181544a0ac
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2025-12-19 20:04:12 -0600

    Changed banner image

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Container, Row, Col, Nav } from 'react-bootstrap';
 import { ArrowRightCircle } from "react-bootstrap-icons";
-import bannerImg from '../assets/img/banner.svg';
+import bannerImg from '../assets/img/banner.png';
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 
