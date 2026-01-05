7dcba2eb8841d4bf46f10ab18c56d6907e1cd8fb
David Xchel Morales Hurtado <davidxchelmh@gmail.com>
2026-01-05 13:35:40 -0600

    Fixed react tests to be able to run

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
+
+// Assign TextEncoder and TextDecoder to the global scope if they are not already defined
+if (!global.TextEncoder) {
+    global.TextEncoder = TextEncoder;
+}
+if (!global.TextDecoder) {
+    global.TextDecoder = TextDecoder;
+}
+// Shorter alternative: Object.assign(global, { TextDecoder, TextEncoder });
